cfe53979407b61e3c705b9dd1c1b06bd10caf962
Igor Varlamov <froginkrit@gmail.ru>
2023-04-25 02:05:11 +0300

    modified the program
    add program for finding the color and determining the coordinates

--- OpenCV/OpenCV/OpenCV.py
+++ OpenCV/OpenCV/OpenCV.py
@@ -1,34 +1,46 @@
 ﻿
 import cv2
+import numpy
 
-# Загружаем изображение
+img = cv2.imread("C:\\Users\Vilgi\Desktop\sdsdw.jpg")   # Загружаем изображение
 
-img = cv2.imread("C:\\Users\Vilgi\Desktop\sdsdw.jpg")
-#img = cv2.imread("E:\\Robo_curling_2023\test_photo\sdsdw.jpg")
+lower_green = (30, 50, 50)                               # Определение диапазона зеленого цвета в HSV
+upper_green = (90, 255, 230)
 
-# Определение диапазона зеленого цвета
-lower_green = (29, 86, 6)
-upper_green = (64, 255, 255)
 
-# Преобразование изображения в цветовую модель HSV
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)              # Преобразование изображения в цветовую модель HSV
 
-# Создание маски для зеленого цвета
-mask = cv2.inRange(hsv, lower_green, upper_green)
+mask = cv2.inRange(hsv, lower_green, upper_green)       # Создание маски для зеленого цвета
 
-# Нахождение контуров на маске
-contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)    # Нахождение контуров на маске
 
-# Вывод координат точек с зеленым цветом
-for cnt in contours:
-    M = cv2.moments(cnt)
-    if M["m00"] != 0:
-        x = int(M["m10"] / M["m00"])
-        y = int(M["m01"] / M["m00"])
-        print("Координаты точки с зеленым цветом: ({}, {})".format(x, y))
 
-# Отображение изображения с маской
-cv2.imshow("Image", img)
-cv2.imshow("Mask", mask)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def mouse_callback(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        pixel_color = img[y, x][::-1]
+        print(f"Цвет пикселя ({x}, {y}): {pixel_color}")
+
+
+def print_():
+    for cnt in contours:                                    # Вывод координат точек с зеленым цветом
+        M = cv2.moments(cnt)
+        if M["m00"] != 0:
+            x = int(M["m10"] / M["m00"])
+            y = int(M["m01"] / M["m00"])
+            print("Координаты точки с зеленым цветом: ({}, {})".format(x, y))
+
+
+def main():
+   
+    cv2.imshow("Mask", mask)                        # Отображение маски
+                
+    cv2.namedWindow('image')                        
+    cv2.setMouseCallback("image", mouse_callback)
+    cv2.imshow('image', img)                        # Отображение окна изображения
+
+    print_()
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()    
+
+main()
