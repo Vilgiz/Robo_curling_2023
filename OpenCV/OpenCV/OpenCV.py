73540ab4ae0d14002f54d6f5540b1b3f69a8bc9f
Igor Varlamov <froginkrit@gmail.ru>
2023-04-26 00:40:23 +0300

    add draw circles

--- OpenCV/OpenCV/OpenCV.py
+++ OpenCV/OpenCV/OpenCV.py
@@ -1,46 +1,64 @@
 ﻿
-import cv2
+import cv2 as cv
 import numpy
 
-img = cv2.imread("C:\\Users\Vilgi\Desktop\sdsdw.jpg")   # Загружаем изображение
+img = cv.imread("C:\\Users\Vilgi\Desktop\sdsdw.jpg")   # Загружаем изображение
 
 lower_green = (30, 50, 50)                               # Определение диапазона зеленого цвета в HSV
 upper_green = (90, 255, 230)
 
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)              # Преобразование изображения в цветовую модель HSV
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)              # Преобразование изображения в цветовую модель HSV
 
-mask = cv2.inRange(hsv, lower_green, upper_green)       # Создание маски для зеленого цвета
+mask = cv.inRange(hsv, lower_green, upper_green)       # Создание маски для зеленого цвета
 
-contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)    # Нахождение контуров на маске
+contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)    # Нахождение контуров на маске
 
 
 def mouse_callback(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
+    if event == cv.EVENT_LBUTTONDOWN:
         pixel_color = img[y, x][::-1]
         print(f"Цвет пикселя ({x}, {y}): {pixel_color}")
 
 
 def print_():
     for cnt in contours:                                    # Вывод координат точек с зеленым цветом
-        M = cv2.moments(cnt)
+        M = cv.moments(cnt)
         if M["m00"] != 0:
             x = int(M["m10"] / M["m00"])
             y = int(M["m01"] / M["m00"])
             print("Координаты точки с зеленым цветом: ({}, {})".format(x, y))
 
+            coordinates = [(x,y)]
+            return coordinates
+
+
+def draw_circles(image, coordinates_list, radius=40, color=(0, 0, 255), thickness=5):
+    for coord in coordinates_list:
+        x, y = coord
+        cv.circle(image, (x, y), radius, color, thickness)
+
+
 
 def main():
    
-    cv2.imshow("Mask", mask)                        # Отображение маски
+    cv.imshow("Mask", mask)                        # Отображение маски 
                 
-    cv2.namedWindow('image')                        
-    cv2.setMouseCallback("image", mouse_callback)
-    cv2.imshow('image', img)                        # Отображение окна изображения
+    cv.namedWindow('image')                        
+    cv.setMouseCallback("image", mouse_callback)
+
+
+
+
+    coordinates = print_()
+
+    draw_circles(img, coordinates)
 
     print_()
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()    
+    cv.imshow('image', img)                        # Отображение окна изображения
+
+    cv.waitKey(0)
+    cv.destroyAllWindows()    
 
 main()
