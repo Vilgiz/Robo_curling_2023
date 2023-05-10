7c342952a1362b5be9199c5849c72353835dcbfd
Igor Varlamov <froginkrit@gmail.ru>
2023-05-10 11:08:54 +0300

    final version

--- OpenCV/OpenCV/Find_piptics.py
+++ OpenCV/OpenCV/Find_piptics.py
@@ -4,16 +4,19 @@ from Camera import Camera
 
 Cap = Camera(1)
 
-red_lower = (170, 50, 50)                                                           # Задаем диапазоны цветов для красного и синего цветов
-red_upper = (180, 255, 255) 
+red_lower = (160, 50, 50)                                                           # Задаем диапазоны цветов для красного и синего цветов
+red_upper = (190, 255, 255) 
 blue_lower = (100, 160, 50)  
 blue_upper = (140, 250, 160)
 
-min_radius = 50                                                                     # Задаем минимальный и максимальный радиусы
-max_radius = 100
+min_radius = 70                                                                     # Задаем минимальный и максимальный радиусы
+max_radius = 150
 
 track_red_pipticks = {}                                                             # МАССИВ С КРАСНЫМИ КАМНЯМИ
 track_id = 0
+
+track_blue_pipticks = {}                                                             # МАССИВ С КРАСНЫМИ КАМНЯМИ
+track_id_blue = 0
 
 red_pipticks_prev_frame = []                            
 blue_pipticks_prev_frame = []
@@ -39,19 +42,23 @@ while True:
 
     for contour in red_contours:                                                    # Отображаем контуры красных кругов на изображении
         (x_red, y_red), radius = cv2.minEnclosingCircle(contour)
-        center = (int(x_red), int(y_red))
+        center_red = (int(x_red), int(y_red))
         radius = int(radius)
         if radius > min_radius and radius < max_radius:
-            cv2.circle(frame, center, radius, (0, 0, 255), 2)
+            cv2.circle(frame, center_red, radius, (0, 0, 255), 2)
             red_pipticks_current_frame.append((int(x_red),int(y_red)))              # запись центра координат во фрейм
 
     for contour in blue_contours:                                                   # Отображаем контуры синих кругов на изображении
         (x_blue, y_blue), radius = cv2.minEnclosingCircle(contour)
-        center = (int(x_blue), int(y_blue))
+        center_blue = (int(x_blue), int(y_blue))
         radius = int(radius)
         if radius > min_radius and radius < max_radius:
-            cv2.circle(frame, center, radius, (255, 0, 0), 2)                       
-            blue_pipticks_current_frame.append((int(x_red),int(y_red)))             # запись центра координат во фрейм
+            cv2.circle(frame, center_blue, radius, (255, 0, 0), 2)                       
+            blue_pipticks_current_frame.append((int(x_blue),int(y_blue)))             # запись центра координат во фрейм
+
+
+
+
 
 
     if count <= 2:
@@ -59,7 +66,7 @@ while True:
             for pt2 in red_pipticks_prev_frame:
                 distance = math.hypot(pt2[0] - pt[0], pt2[1] - pt[1])
                 
-                if distance < 20:                                                   # размер погрешности движения одного и того же объекта
+                if distance < 30:                                                   # размер погрешности движения одного и того же объекта
                     track_red_pipticks[track_id] = pt
                     track_id += 1
 
@@ -72,7 +79,7 @@ while True:
             for pt in red_pipticks_current_frame_copy:
                 distance = math.hypot(pt2[0] - pt[0], pt2[1] - pt[1])
                 # Обновлениие позиции камня
-                if distance < 20:                                                   # размер погрешности движения одного и того же объекта
+                if distance < 30:                                                   # размер погрешности движения одного и того же объекта
                     track_red_pipticks[obj_id] = pt
                     obj_exists = True
                     red_pipticks_current_frame.remove(pt)
@@ -82,22 +89,64 @@ while True:
                 track_red_pipticks.pop(obj_id) # add list
                     
     for obj_id, pt in track_red_pipticks.items():
-        cv2.circle(frame, center, 1, (0, 255, 0), -1)
+        cv2.circle(frame, center_red, 1, (0, 255, 0), -1)
         cv2.putText(frame, str(obj_id), (pt[0], pt[1] - 7), 0, 1, (0,0,255), 2)
 
-    print("#################")
+
+########################################################################################### 
+
+    if count <= 2:
+        for pt_blue in blue_pipticks_current_frame:
+            for pt2_blue in blue_pipticks_prev_frame:
+                distance_blue = math.hypot(pt2_blue[0] - pt_blue[0], pt2_blue[1] - pt_blue[1])
+                
+                if distance_blue < 20:                                                   # размер погрешности движения одного и того же объекта
+                    track_blue_pipticks[track_id_blue] = pt_blue
+                    track_id_blue += 1
+
+    else: 
+        track_blue_pipticks_copy = track_blue_pipticks.copy()
+        blue_pipticks_current_frame_copy = blue_pipticks_current_frame.copy()
+
+        for obj_id_blue, pt2_blue in track_blue_pipticks_copy.items():
+            obj_exists_blue = False
+            for pt_blue in blue_pipticks_current_frame_copy:
+                distance_blue = math.hypot(pt2_blue[0] - pt_blue[0], pt2_blue[1] - pt_blue[1])
+                # Обновлениие позиции камня
+                if distance_blue < 20:                                                   # размер погрешности движения одного и того же объекта
+                    track_blue_pipticks[obj_id_blue] = pt_blue
+                    obj_exists_blue = True
+                    blue_pipticks_current_frame.remove(pt_blue)
+                    continue
+                # Удаление id
+            if not obj_exists_blue:
+                track_blue_pipticks.pop(obj_id_blue) # add list
+                   
+
+
+    for obj_id_blue, pt_blue in track_blue_pipticks.items():
+        cv2.circle(frame, center_blue, 1, (0, 255, 0), -1)
+        cv2.putText(frame, str(obj_id_blue), (pt_blue[0], pt_blue[1] - 7), 0, 1, (0,0,255), 2)
+#######################################################################################
+
+    print("###########################")
+
+    print("RED PiPticks:")
     print(track_red_pipticks)
+    print("BLUE PiPticks:")
+    print(track_blue_pipticks)
 
+    print("###########################")
 
-    print("curr frame")
-    print(red_pipticks_current_frame)
+  #  print("curr frame")
+   # print(red_pipticks_current_frame)
 
     cv2.imshow('frame', frame)                                                   
     
     red_pipticks_prev_frame = red_pipticks_current_frame.copy()
     blue_pipticks_prev_frame = blue_pipticks_current_frame.copy()
     
-    cv2.waitKey(1)
+    cv2.waitKey(0)
 
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
